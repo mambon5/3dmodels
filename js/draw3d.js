e79f4824dfda257da20e4f1cf324961259356773
roma <romdommas@gmail.com>
2024-07-07 16:47:58 +0200

    added a few extra wooden sticks to make the model better

--- js/draw3d.js
+++ js/draw3d.js
@@ -95,52 +95,75 @@ function dibuix_rectangle(rectangle, a,b,c) {
 function model_viscaSofa(x,y,z) {
     // x, y, z, ample, alt, profund
     //travesanys profund terra    
-    rect1 = rectangle3d(x,y,z,4,4,50);
-    rect2 = rectangle3d(x+50,y,z,4,4,50);
-    rect3 = rectangle3d(x+50+50,y,z,4,4,50);
-    rect4 = rectangle3d(x+50+50+50,y,z,4,4,50);
-    rect4_2 = rectangle3d(x+200,y,z,4,4,50);
+    recttravpf1 = rectangle3d(x,y,z,4,4,50);
+    recttravpf2 = rectangle3d(x+50,y,z,4,4,50);
+    recttravpf3 = rectangle3d(x+50+50,y,z,4,4,50);
+    recttravpf4 = rectangle3d(x+50+50+4+2,y,z,4,4,50);
+    recttravpf5 = rectangle3d(x+50+50+50+4+2,y,z,4,4,50);
+    recttravpf6 = rectangle3d(x+204+2,y,z,4,4,50);
     
-    //travesanys llargs 
-    // rect5 = rectangle3d(x,y,z+50,124,4,4);
-    rect6 = rectangle3d(x,y+20,z+50,205,4,4);
+    
 
     // pal amun petit frontal
-    rect7 = rectangle3d(x,y,z+50,4,20,4);
-    rect8 = rectangle3d(x+50,y,z+50,4,20,4);
-    rect8v1 = rectangle3d(x+100,y,z+50,4,20,4);
-    rect8v2 = rectangle3d(x+150,y,z+50,4,20,4);
-    rect8v3 = rectangle3d(x+200,y,z+50,4,20,4);
+    let alt_y=44
+    rect7 = rectangle3d(x,y,z+50,4,alt_y,4);
+    rect8 = rectangle3d(x+50,y,z+50,4,alt_y,4);
+    rect8v1 = rectangle3d(x+100,y,z+50,4,alt_y,4);
+    rect8v2 = rectangle3d(x+104+2,y,z+50,4,alt_y,4);
+    rect8v3 = rectangle3d(x+154+2,y,z+50,4,alt_y,4);
+    rect8v4 = rectangle3d(x+204+2,y,z+50,4,alt_y,4);
+
+    // pal amun petit darrere
+    rectampet1 = rectangle3d(x,y,z-4+10,4,alt_y,4);
+    rectampet2 = rectangle3d(x+50,y,z-4+10,4,alt_y,4);
+    rectampet3 = rectangle3d(x+100,y,z-4+10,4,alt_y,4);
+    rectampet4 = rectangle3d(x+104+2,y,z-4+10,4,alt_y,4);
+    rectampet5 = rectangle3d(x+154+2,y,z-4+10,4,alt_y,4);
+    rectampet6 = rectangle3d(x+204+2,y,z-4+10,4,alt_y,4);
+
+    //travesanys llargs 
+    // rect5 = rectangle3d(x,y,z+50,124,4,4);
+    rectravll1 = rectangle3d(x,y+alt_y,z+50,104,4,4);
+    rectravll2 = rectangle3d(x+104+2,y+alt_y,z+50,104,4,4);
+    rectravll3 = rectangle3d(x,y+alt_y,z-4+10,104,4,4);
+    rectravll4 = rectangle3d(x+104+2,y+alt_y,z-4+10,104,4,4);
 
     // pal amunt llarg darrere
     rectAll1 = rectangle3d(x,y,z-4,4,90,4);
     rectAll2 = rectangle3d(x+50,y,z-4,4,90,4, "red");
     rectAll3 = rectangle3d(x+100,y,z-4,4,90,4);
-    rectAll4 = rectangle3d(x+150,y,z-4,4,90,4);
-    rectAll5 = rectangle3d(x+200,y,z-4,4,90,4);
+    rectAll4 = rectangle3d(x+104+2,y,z-4,4,90,4);
+    rectAll5 = rectangle3d(x+154+2,y,z-4,4,90,4);
+    rectAll6 = rectangle3d(x+204+2,y,z-4,4,90,4);
     
     
     // pals inclinats llargs respaldo
     //respado vs asiento debe manteren 110-120 grados de inclinacion, means 60 wrt vertical
+    let ang_x = -6
     let rectIll1 = [rotate_rectangle_from_point(
-        rectangle3d(x+0,y,z-4+15, 4, 90, 4)[0], -10,0,0,[x+0,y,z-4+10])]
+        rectangle3d(x+4,y,z-4+10, 4, 90, 4)[0], ang_x,0,0,[x+0+4,y,z-4+10])]
     let rectIll2 = [rotate_rectangle_from_point(
-        rectangle3d(x+50,y,z-4+15, 4, 90, 4)[0], -10,0,0,[x+50,y,z-4+10])]
+        rectangle3d(x+50-4,y,z-4+10, 4, 90, 4)[0], ang_x,0,0,[x+50-4,y,z-4+10])]
     let rectIll3 = [rotate_rectangle_from_point(
-        rectangle3d(x+100,y,z-4+15, 4, 90, 4)[0], -10,0,0,[x+100,y,z-4+10])]
+        rectangle3d(x+100-4,y,z-4+10, 4, 90, 4)[0], ang_x,0,0,[x+100-4,y,z-4+10])]
     let rectIll4 = [rotate_rectangle_from_point(
-        rectangle3d(x+150,y,z-4+15, 4, 90, 4)[0], -10,0,0,[x+150,y,z-4+10])]
+        rectangle3d(x+104+2+4,y,z-4+10, 4, 90, 4)[0], ang_x,0,0,[x+104+2+4,y,z-4+10])]
     let rectIll5 = [rotate_rectangle_from_point(
-        rectangle3d(x+200,y,z-4+15, 4, 90, 4)[0], -10,0,0,[x+200,y,z-4+10])]
+        rectangle3d(x+154+2-4,y,z-4+10, 4, 90, 4)[0], ang_x,0,0,[x+154+2-4,y,z-4+10])]
+    let rectIll6 = [rotate_rectangle_from_point(
+        rectangle3d(x+204+2-4,y,z-4+10, 4, 90, 4)[0], ang_x,0,0,[x+204+2-4,y,z-4+10])]
     
-    var model3d = [rect1, rect2, rect3, rect4,rect4_2,rect6,rect7,rect8,rect8v1,
-        rect8v2,rect8v3, rectAll1, rectAll2, rectAll3, rectAll4, rectAll5, rectIll1, 
-        rectIll2, rectIll3, rectIll4, rectIll5]
+    var model3d = [recttravpf1, recttravpf2, recttravpf3, recttravpf4,
+        recttravpf5, recttravpf6,rectravll1, rectravll2,rectravll3, rectravll4,rect7,rect8,rect8v1,
+        rect8v2,rect8v3, rect8v4,rectAll1, rectAll2, rectAll3, rectAll4, rectAll5, rectAll6,
+         rectIll1, 
+        rectIll2, rectIll3, rectIll4, rectIll5, rectIll6,
+        rectampet1, rectampet2, rectampet3, rectampet4, rectampet5, rectampet6]
     return model3d
 }
 
 function dibuixa(a,b,c) {
-    x =-90; y =20; z = 0 // punt inicial en 3D
+    x =-90; y =-10; z = -10 // punt inicial en 3D
     //a,b,c angles de rotació
 
     model3d = model_viscaSofa(x, y, z)  
